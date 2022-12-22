57610eb928157216966dbff3e2b03cf225d0a124
howdyfrom2019 <tjd1emd@naver.com>
2022-12-22 19:11:32 +0900

    feat: mouse interaction

--- js/App.js
+++ js/App.js
@@ -3,6 +3,8 @@ import DownCard from "./DownCard.js";
 
 export default function App($app) {
   this.$card =  document.createElement("div");
+  this.mouseX = 0;
+  this.mouseY = 0;
   this.state = {
 
   }
@@ -24,9 +26,19 @@ export default function App($app) {
     onClick: (e) => {
       console.log(e);
     }
-  })
+  });
 
-  this.render = () => {
-    $app.innerHTML = this.$card;
+  this.mouseInteractive = () => {
+    window.addEventListener("mousemove", (e) => {
+      this.mouseX = e.clientX - (window.innerWidth / 2);
+      this.mouseY = e.clientY - (window.innerHeight / 2);
+      this.rotateCard();
+    }, false);
   }
+
+  this.rotateCard = () => {
+    this.$card.style.transform = `translate(-50%, -48%) rotateZ(${(this.mouseX + this.mouseY) / 100}deg) rotateY(${this.mouseX / 100}deg) rotateX(${-this.mouseY / 100}deg) ${window.innerWidth < 768 ? 'scale(0.14)' : 'scale(0.3)'}`;
+  }
+
+  this.mouseInteractive();
 };
